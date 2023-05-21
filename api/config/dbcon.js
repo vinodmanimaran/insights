234480234ea9cf92b72bcf70a98d7fdefba33ef2
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-21 23:22:10 +0530

    Update dbcon.js

--- api/config/dbcon.js
+++ api/config/dbcon.js
@@ -1,13 +1,16 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
+const env=require('dotenv').config();
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect (process.env.DATABASE_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
   } catch (err) {
-    console.log (err);
+    console.error(err);
   }
 };
 
