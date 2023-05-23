972cd348f9c5d704e8c16269f2ef14cd06350a26
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-24 01:21:56 +0530

    Update Post.jsx

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,15 +1,18 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
-import './post.css'
+import './post.css';
 
-export default function Post({post}) {
+export default function Post({ post }) {
   const PF = 'https://blog-api-cvnk.onrender.com/images/';
+
+  const formattedDate = moment(post.createdAt).format('MMMM DD, YYYY');
+
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          {post.categories.map (c => (
+          {post.categories.map(c => (
             <span className="postCat" key={c._id}>{c.name}</span>
           ))}
         </div>
@@ -18,7 +21,7 @@ export default function Post({post}) {
         </Link>
         <hr />
         <span className="postDate">
-          {moment (post.createdAt).format ('MMMM DD, YYYY')}
+          {formattedDate}
         </span>
       </div>
       <p className="postDesc">{post.desc}</p>
